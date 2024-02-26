423b66f6e1b99f0324061684944a8b6ad4fb06f9
AyeshaNasirWebDeveloper <ayeshanasir806@gmail.com>
2024-02-26 20:39:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/*Question Number. 01
+
+Create a const object called "Product" to store information shown in the picture.*/
+
+const product = {
+    title: "Ball Pen",
+    rating: 4.5,
+    offer: 5,
+    price: 370,
+    isDeal: true,
+}
+
+console.log(product);
+
+/*Question Number. 02
+
+Create a const object called "Profile" to store information shown in the picture.*/
+
+const profile={
+    userName: "Ayesha Nasir",
+    isFollow: false,
+    followers: 546,
+    following: 685,
+}
+
+console.log(profile);
